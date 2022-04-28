45b449c8f8ce81e009a0c9698aaeac1062fc9bce
miker704 <miker704@gmail.com>
2022-04-28 17:11:43 -0400

    associations project update

---
+++ Associations_Excercise_rails/Associations_Excercise_rails/skeleton/app/models/course.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint           not null, primary key
+#  name          :string
+#  prereq_id     :integer
+#  instructor_id :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+class Course < ApplicationRecord
+
+  
+    has_many(
+        :enrollments,
+        class_name: 'Enrollment',
+        foreign_key: :course_id,
+        primary_key: :id
+    )
+    # has_many(
+    #     :users,
+    #     class_name: 'User',
+    #     foreign_key: :user_id,
+    #     foreign_key: :instructor_id,
+    #     primary_key: :id
+    # )
+end
---
+++ Associations_Excercise_rails/Associations_Excercise_rails/skeleton/app/models/user.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class User < ApplicationRecord
+ 
+    has_many :enrollments,
+        class_name: 'Enrollment',
+        foreign_key: :student_id,
+        primary_key: :id
+    
+
+
+    # has_many(
+    #     :courses,
+    #     class_name: 'Course',
+    #     foreign_key: :course_id,
+    #     foreign_key: :instructor_id,
+    #     primary_key: :id
+    # )
+    
+
+end
